9ca66637eebf564396ae917c3a2129f9877e789c
Jacobo Tomas Lerner Rojas <j.lernerrojas@gmail.com>
2023-03-13 08:46:21 -0300

    se aplico arrays,objetos y nuevo algoritmo diagnostico

--- main.js
+++ main.js
@@ -1,26 +1,68 @@
-//la primera parte del simulador permite al usuario ingresar ciertos sintomas para poder llegar a un diagnostico y tratamiento dependiendo de los datos ingresados, si no se logra un hilo de datos correctos se llega a un no diagnostico, al ingresar los sintomas, se dispuso el codigo para que no tenga una orden en particular
-
-
-function medico ( sintoma1, sintoma2, sintoma3){
-    var sintoma1 = prompt ("Ingresar un sintoma:");
-    var sintoma2 = prompt ("Ingresar otro sintoma: ");
-    var sintoma3 = prompt ("Ingresar otro sintoma:");
-    if ((( sintoma1 == "diarrea")||( sintoma1 == "vomitos")||( sintoma1 == "dolor abdominal")) && (( sintoma2 == "diarrea")||( sintoma2 == "vomitos")||( sintoma2 == "dolor abdominal")) && (( sintoma3 == "diarrea")||( sintoma3 == "vomitos")||( sintoma3 == "dolor abdominal"))){
-        alert("Usted esta cursando una gastroenteritis, haga dieta y tome reliveran");
-    } 
-    else if((( sintoma1 == "fiebre")||( sintoma1 == "dolor de garganta")||( sintoma1 == "congestion nasal")) && (( sintoma2 == "fiebre")||( sintoma2 == "congestion nasal")||( sintoma2 == "dolor de garganta")) && (( sintoma3 == "fiebre")||( sintoma3 == "congestion nasal")||( sintoma3 == "dolor de garganta"))){
-        alert("Usted esta cursando una faringitis, tome paracetamol y acemuk");
+//se declara clase para poder construir objetos, incluyen sintomas, nombre de patologia y pautas a tomar con diagnostico de dicha patologia.
+class cuadroClinico {
+    constructor(nombre,pautas, sintoma1, sintoma2, sintoma3){
+        this.nombre= nombre
+        this.pautas= pautas
+        this.sintoma1= sintoma1
+        this.sintoma2= sintoma2
+        this.sintoma3= sintoma3
     }
-    else if((( sintoma1 == "dolor cervical")||( sintoma1 == "mareos")||( sintoma1 == "dolor de cabeza")) && (( sintoma2 == "dolor cervical")||( sintoma2 == "mareos")||( sintoma2 == "dolor de cabeza")) && (( sintoma3 == "dolor cervical")||( sintoma3 == "mareos")||( sintoma3 == "dolor de cabeza"))){
-        alert("Usted esta cursando una cervicalgia, tome diclofenac y haga reposo");
+}
+const cuadro1 = new cuadroClinico ( "gastroenteritis", "hidratarse, tomar agua y tomar antidiarreicos","dolor abdominal", "vomitos", "diarrea");
+const cuadro2 = new cuadroClinico ( "faringitis", "tomar ibuprofeno y comprimidos de ernex","fiebre", "dolor de garganta", "congestion nasal");
+const cuadro3 = new cuadroClinico( "cervicalgia","tomar diclofenac y aplicar calor en la zona","dolor de cuello", "mareos", "dolor de cabeza");
+
+//se construye array a base de cuadros clinicos, se puede agregar cuadros de forma mas rapida y eficaz que primer entrega
+const arraycuadroClinico = [cuadro1, cuadro2, cuadro3];
+
+//se armar tres funciones para el ingreso de sintomas via prompt que discriminan respuesta en blanco
+function solicitarSintoma1(){
+    let sintoma1 = prompt ("Ingresar un sintoma:");
+    while (sintoma1 === ""){
+        sintoma1 = prompt ("Ingresar un sintoma valido:");
     }
-    else{
-        alert("No se puedo llegar a un diagnostico, acuda a la guardia de forma presencial");
-    } 
-    return "gracias por usar asistencia virtual"
+    return sintoma1;}
+
+function solicitarSintoma2(){
+        let sintoma2 = prompt ("Ingresar otro sintoma: ");
+        while (sintoma2 === ""){
+            sintoma2 = prompt ("Ingresar un sintoma valido:");
+        }
+        return sintoma2;}
+
+function solicitarSintoma3(){
+            let sintoma3 = prompt ("Ingresar otro sintoma:");
+            while (sintoma3 === ""){
+                sintoma3 = prompt ("Ingresar un sintoma valido:");
+            }
+            return sintoma3;}
+
+let sintoma1=solicitarSintoma1();
+let sintoma2 =solicitarSintoma2();
+let sintoma3 =solicitarSintoma3();
+
+//estas funciones de orden superiores aisla el objeto dentro del array que tenga los sintomas referidos, fue complejo armar esto, de manera que esta redactado es imposible que los sintomas se mezclen y para llegar al diagnostico se debe tener el cuadro clinico completo
+const resultadoClinico = arraycuadroClinico.filter((sintoma)=>((sintoma.sintoma1.includes(sintoma1))||(sintoma.sintoma1.includes(sintoma2))||(sintoma.sintoma1.includes(sintoma3))
+)&&((sintoma.sintoma2.includes(sintoma1))||(sintoma.sintoma2.includes(sintoma2))||(sintoma.sintoma2.includes(sintoma3)))&&((sintoma.sintoma3.includes(sintoma1))||(sintoma.sintoma3.includes(sintoma2))||(sintoma.sintoma3.includes(sintoma3))))
+
+//este aisla el nombre del cuadro si es posible el diagnostico
+const resultadoDiagnostico= resultadoClinico.map((el) => el.nombre)
+
+//este aisla las pautas de alarma si existe
+const resultadoPautas= resultadoClinico.map((el) => el.pautas)
+
+//mensaje con pautas de alarma y daignostico
+let mensaje = " Usted esta cursando un cuadro de " + resultadoDiagnostico + " las medidas a tomar son " + resultadoPautas
+
+//si se logra tener un array con un diagnostico( es decir un array que tenga un nombre) se da una alerta, en caso contrario se da un mensaje de no diagnostico
+if ((resultadoDiagnostico.some((el) =>(el.nombre == "faringitis"||"gastroenteritis"||"cervicalgia")))){
+    alert (mensaje);
+    console.log(mensaje)
+}else {
+    alert (" No se logro llegar a un diagnostico, acudir a la guardia")
 }
 
-medico ();
+
 //luego se le otorga la oportunidad de aceptar o no la asistencia y a base de eso dar por finalizada la atencion o mandar una queja por mail
 
 function ingresaAcepta(){
@@ -43,6 +85,3 @@ function ingresaAcepta(){
 }
 
 ingresaAcepta();
-
-
-
